9bba6a41abc43d0ab1ae22c5c57a80888f5a0896
Anuska <anuska.mridha08@gmail.com>
2026-05-15 07:24:08 +0530

    Updated project files

--- api/index.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Add the app directory to the path
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
-
-from app.app import app
-
-# Export the app for Vercel
